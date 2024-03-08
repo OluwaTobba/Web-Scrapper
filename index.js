ad0a6c6b4b61303c8c97ff30a1516540238f9b7f
OluwaTobba <142856021+OluwaTobba@users.noreply.github.com>
2024-03-08 10:38:48 +0100

    index.js

--- index.js
+++ index.js
@@ -2,13 +2,32 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 const fs = require('fs');
 
-const url = "https://www.imdb.com/chart/top/";
+const url = "https://www.imdb.com/chart/top/?ref_=nv_mv_250";
 
 const moviesData = {};
 
+// async function getHTML () {
+//     const { data: html } = await axios.get(url);
+//     return html;
+// };
+
+// async function getHTML() {
+//     try {
+//         const { data: html } = await axios.get(url);
+//         return html;
+//     } catch (error) {
+//         throw new Error(`Error fetching HTML: ${error.message}`);
+//     }
+// }
+
+
 async function getHTML() {
     try {
-        const { data: html } = await axios.get(url);
+        const { data: html } = await axios.get(url, {
+            headers: {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+            },
+        });
         return html;
     } catch (error) {
         throw new Error(`Error fetching HTML: ${error.message}`);
@@ -18,8 +37,8 @@ async function getHTML() {
 getHTML().then((res) => {
     const $ = cheerio.load(res);
     $('.lister-list>tr').each((i, movie) => {
-        const title = $(movie).find('.titleColumn a').text();
-        const rating = $(movie).find('.imdbRating strong').text();
+        const title = $(movie).find('.ipc-title--base').text();
+        const rating = $(movie).find('.ipc-rating-star--imdb').text();
         moviesData[title] = rating;
     });
 
